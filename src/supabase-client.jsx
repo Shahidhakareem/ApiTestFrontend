a63b23c3b0227b7793033169d464f3d009c02d47
Shahidhakareem <shahidhakareem134@gmail.com>
2025-12-05 15:30:57 +0530

    Update supabase-client.jsx

--- src/supabase-client.jsx
+++ src/supabase-client.jsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://ddoucwlbhlgctdegrmaz.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkb3Vjd2xiaGxnY3RkZWdybWF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5NzA1NzMsImV4cCI6MjA3OTU0NjU3M30.6mBVe7FcogDDxVCUvbhyaoSpU8Na10ExVwZcuPPmmFU"
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 );
